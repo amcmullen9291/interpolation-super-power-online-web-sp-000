574f1cb2b48b9dfa3dcdc025c16b73e4076942d9
anthony mcmullen <jondouglasheit@gmail.com>
2020-05-14 17:01:32 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,7 +1,7 @@
 def display_rainbow(colors)
   x = 0
   while x < colors.length do 
-   puts "#{colors[x](0).uppercase}: #{colors[x]}"
+   puts "#{colors[x][0].uppercase}: #{colors[x]}"
  end
  x+=1 
 end 
