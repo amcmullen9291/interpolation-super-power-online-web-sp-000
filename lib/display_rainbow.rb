27b882605f50e69a9cddac1b20a203c754c57d7b
anthony mcmullen <jondouglasheit@gmail.com>
2020-05-14 17:07:53 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,6 +1,6 @@
 def display_rainbow(colors)
   x = 0
-  until x = colors.length do 
+  until x >= colors.length do 
     if x == colors.length
       puts "#{colors[x][0].capitalize}: #{colors[x]}\n"
     else
